ae299edd979d493f44855a493dd80396022e0393
Richard Dotchin <richard.dotchin@gmail.com>
2017-04-10 19:07:49 -0400

    added routes and mongoose

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,3 +1,35 @@
-/**
- * Created by RDotchin on 4/10/17.
- */
+const db = require('./../models/database.js');
+const Blog = require('./../blogModel.js');
+module.exports = (app) => {
+
+    app.post('/api/create', (req, res) => {
+        console.log(req.body);
+        //add blog data**
+        var newBlog = new Blog({});
+        createBlog.save((err, blog) => {
+            if(err) throw err;
+            res.send(200);
+        })
+    });
+
+    //READ
+    app.get('/api/blog', (req, res) => {
+        db.find({}, (err, data) => {
+            console.log(data);
+            res.json(data);
+        });
+    });
+
+    //UPDATE
+    app.post('api/update', (req, res) => {
+        console.log(req.body);
+        db.findByIdAndUpdate({}, (err, data) => {
+            console.log(data);
+        });
+    });
+
+    //DELETE
+    app.get('api/delete/', (req, res) => {
+        /*db.find({}).remove().exec();*/
+    })
+};
